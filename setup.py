395e9b0bd79959dcb79e1eba77b585a2dda1d0ae
Marcos <70981547+marcosporteiro@users.noreply.github.com>
2023-08-07 01:21:47 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='RuletaEuropea',
-    version='0.1.4',
+    version='0.1.5',
     packages=find_packages(),
     author='Marcos Porteiro',
     description='Implementacion de la ruleta europea hecha en python',  
