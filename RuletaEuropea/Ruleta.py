0c13710df9f33c57ec777634c635719055aa9a39
Marcos <70981547+marcosporteiro@users.noreply.github.com>
2023-08-07 01:45:32 -0300

    Add files via upload

--- RuletaEuropea/Ruleta.py
+++ RuletaEuropea/Ruleta.py
@@ -9,6 +9,17 @@ class Ruleta:
         self.apuestas = []
         self.bola = None
 
+    def tablero(self):
+        return """                      Calle    
+     01 02 03 04   05 06 07 08  09  10 11 12    
+ / | [2;31m03[0m 06 [2;31m09 12[0m | 15 [2;31m18 21[0m 24 | [2;31m27 30[0m 33 [2;31m36[0m | Columna3 
+ [2;36m0[0m | 02 [2;31m05[0m 08 11 | [2;31m14[0m 17 20 [2;31m23[0m | 26 29 [2;31m32[0m 35 | Columna2
+ \ | [2;31m01[0m 04 [2;31m[2;31m07[0m[2;31m[0m 10 | 13 [2;31m16 19[0m 22 | [2;31m25[0m 28 31 [2;31m34[0m | Columna1 
+       1Docena       2Docena       3Docena
+        01-18       Par-Impar       19-36
+       (Bajos)     [2;31mRojo[0m-Negro      (Altos)  
+"""
+
     def caer_bola(self):
         self.bola = random.choice(self.numeros)
         return self.bola
